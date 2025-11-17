6e9c9e648c73757da3907ce681ccad84c07ce2a2
chandrikahatti <138065963+chandrikahatti@users.noreply.github.com>
2025-11-16 22:57:00 -0600

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+### **9. File: `.gitignore`** (Root folder)
+```
+node_modules
+dist
+.env
+.DS_Store
